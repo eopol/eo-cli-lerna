845b0324e825da6ee4159b0d3f18fa5bdc7e8525
i7eo <admin@i7eo.com>
2022-08-29 18:28:28 +0800

    bump version

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -4,5 +4,5 @@ module.exports = core;
 
 function core() {
     // TODO
-    console.log("@eo-cli/core")
+    console.log("@eo-cli/core.")
 }
--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -4,5 +4,5 @@ module.exports = utils;
 
 function utils() {
     // TODO
-    console.log("@eo-cli/utils")
+    console.log("@eo-cli/utils.")
 }
